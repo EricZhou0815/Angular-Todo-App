a3250bbe40d9e10663885fc8ebb83ca5c0a57c4d
Eric Zhou <eric.zhou0815@gmail.com>
2019-04-05 21:32:59 +0800

    update file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-todo-app'`, () => {
+  it(`should have as title 'AngularTodoApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-todo-app');
+    expect(app.title).toEqual('AngularTodoApp');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-todo-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('TodoList');
   });
 });
